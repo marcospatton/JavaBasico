05901a81f3dcc60670c360fd4eb5948f6f076ccd
Marco Antonio da Silva Francisco <marcospatton@hotmail.com>
2021-12-20 19:34:57 -0300

    Add files via upload

---
+++ Testaconversao.java
@@ -0,0 +1,32 @@
+
+public class Testaconversao {
+	
+	public static void main(String[] args) {
+		double salario = 1270.50;
+		int valor = (int) salario;
+		System.out.println(valor);
+		
+		long numeroGrande = 3325656565321231l;
+		
+		double valor1 = 0.2;
+		double valor2 = 0.1;
+		double total = valor1 + valor2;
+		
+		System.out.println(total);
+		
+		int idade = 20;
+		System.out.println("A idade de Marcos é 30 !");
+		
+		int trinta = 10 + 20;
+		System.out.println("A idade de Marcos é " + trinta);
+		
+		double idade = 30.0;
+		System.out.println("A idade de Marcos é " + (int) idade + "!");
+		
+		int idade = 3 * 10;
+			System.out.println("A idade de Marcos é " + idade + "!");
+		
+		int idade = 30;
+		System.out.println("A idade de Marcos é " + idade + "!");
+	}
+}

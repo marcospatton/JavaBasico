ed6ee38d9eac96fe033b5fe060080496c13a94a6
Marco Antonio da Silva Francisco <marcospatton@hotmail.com>
2021-12-20 13:17:58 -0300

    Adicionado teste de variaveis
    Adicionado teste de variaveis

---
+++ TestaVariaveis.java
@@ -0,0 +1,25 @@
+
+public class TestaVariaveis {
+	
+	public static void main(String[] args) {
+		System.out.println("ola novo teste");
+		
+		int idade;
+		idade = 41;
+		
+		System.out.println(idade);
+		
+		idade = 30 + 10;
+		
+		System.out.println(idade);
+		
+		idade = (7 * 5) + 2;
+		
+		System.out.println(idade);
+		
+		System.out.println("a idade é " + idade + " Parabéns ");
+		
+		System.out.print(" A idade é  " + idade + " parabéns ");
+	}
+
+}

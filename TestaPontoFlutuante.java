f1989664bb9eceb72cd5ef20ef441b78f66f3d4e
Marco Antonio da Silva Francisco <marcospatton@hotmail.com>
2021-12-20 18:28:45 -0300

    Add files via upload

---
+++ TestaPontoFlutuante.java
@@ -0,0 +1,42 @@
+
+public class TestaPontoFlutuante {
+	
+	public static void main(String[] args) {
+		double salario;
+		salario = 1250.70;
+		System.out.println("meu salário é " + salario);
+		
+		double idade = 37;
+		
+		double divisao = 3.14 /2;
+		System.out.println(divisao);
+		
+		int outraDivisao = 5 / 2 ;
+		System.out.println(outraDivisao);
+		
+		double novaTentativa = 5.0 / 2;
+		System.out.println(novaTentativa);
+		
+		int dia = 4;
+		int outroDia = 4 + dia;
+		System.out.println(outroDia);
+		
+		int brindes = 15;
+		int pessoas = 2;
+		System.out.println(brindes / pessoas);
+		
+		double peso = 4.0;
+		int quantidade = 10;
+		System.out.println(peso * quantidade);
+		
+		double preco = 5.5;
+		int ingressos = 4;
+		System.out.println(preco * ingressos);
+		
+		
+	
+		
+		
+		
+	}
+}

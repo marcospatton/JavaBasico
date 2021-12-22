e63856e36f62b13efb319d272422fdadd3764870
Marco Antonio da Silva Francisco <marcospatton@hotmail.com>
2021-12-22 16:45:27 -0300

    Add files via upload

--- TestaCondicional2.java
+++ TestaCondicional2.java
@@ -1,25 +1,19 @@
-
 public class TestaCondicional2 {
-	public static void main(String[] args) {
-		System.out.println("testando condicionais");
-		int idade = 16;
-		int quantidadePessoas = 3;
-		
-		if (idade >= 18 || quantidadePessoas >= 2) {
-			System.out.println("você tem mais de 18 anos");
-			System.out.println("seja vem vindo");
-		
-		
-	}else {
-		if(quantidadePessoas >= 2) {
-			System.out.println("voce não tem 18 anos, mas pode entrar, "
-					+ "pois esta acompanhado");
-		}else {
-			
-		}
-		System.out.println("infelismente voce não pode entrar");
-	}
+    public static void main(String[] args) {
+        System.out.println("testando condicionais");
+        int idade = 20;
+                int quantidadePessoas = 3;
+        boolean acompanhado = quantidadePessoas >= 2;
 
-  }
+                System.out.println("valor de acompanhado = " + acompanhado);
 
+        if (idade >= 18 && acompanhado) {
+            System.out.println("seja bem vindo");
+        } else {
+            System.out.println("infelizmente você não pode entrar");
+            int idade2 = 68;
+            boolean ehIdoso = idade2>= 65;
+        }
+        
+    }
 }
